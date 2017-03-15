33b10d937d172df116f0404f965326dc87c8c38c
Hanna <github email address>
2017-03-15 16:58:51 -0400

    albums list is now unique and more questions added to triviasection

--- app/controllers/albums_controller.rb
+++ app/controllers/albums_controller.rb
@@ -16,12 +16,20 @@ class AlbumsController < ActionController::Base
       @artist_message = "Try Another Name"
     else
       @artist = RSpotify::Artist.search(params[:search]).first
-      @albums = fetch_random_five(@artist.top_tracks(:US).map{|track| track.album })
+      # @albums = fetch_random_five(@artist.top_tracks(:US).map{|track| track.album })
+
+      @albums = unique_albums(@artist.albums).sample(5)
+
+      # @albums_unique = @albums.uniq
+      # hash.map { |k,v| v }.uniq
+
 
       if @albums.nil?
-        @artist_message = "Try Another Aame"
+        @artist_message = "Try Another Name"
       end
       @album_questions = @albums.map{|album| Album.new(album)}
+
+      # binding.pry
 
       render :questions and return
     end
@@ -40,9 +48,6 @@ class AlbumsController < ActionController::Base
       @message2 =  "Here are the answers:"
 
       @message3 = "#{params[:track]} is on Spotify's #{RSpotify::Track.search(params[:track]).first.album.name} album!"
-      # binding.pry
-
-      # @message += "You guessed #{params[:track]}"
 
       year_album = RSpotify::Album.find(params[:release_year_id])
       @message4 = "You guessed " + year_album.name + " was released in " + params[:release_year] + ".\n"
@@ -50,23 +55,46 @@ class AlbumsController < ActionController::Base
       popularity_album = RSpotify::Album.find(params[:popularity_rate_id])
       @message6 = "You guessed #{params[:popularity_rate]} for #{popularity_album.name}'s popularity on Spotify\n"
       @message7 = "The answer was: #{popularity_album.popularity}\n"
-      # "Bravo!" + params[:track].values[0] + "is on the "
-      # if params[:track] && (params[:track] == "True" || params[:track] == "true")
-        # ""
-      # render :home
+
+      sec_year_album = RSpotify::Album.find(params[:sec_release_year_id])
+      sec_year_album_name = sec_year_album.name
+      @message8 = "You guessed " + sec_year_album_name + " was released in " + params[:sec_release_year] + ".\n"
+      @message9 = "#{sec_year_album_name} was released in #{sec_year_album.release_date.to_i}.\n"
+      sec_popularity_album = RSpotify::Album.find(params[:sec_popularity_rate_id])
+      sec_popularity_album_name = sec_popularity_album.name
+      @message10 = "You guessed #{params[:sec_popularity_rate]} for #{sec_popularity_album_name}'s popularity on Spotify\n"
+      @message11 = "The answer was: #{sec_popularity_album.popularity}\n"
     end
 
 
 
   private
 
-    def fetch_random_five(array)
-      array.sample(5)
+    def unique_albums(array)
+      # array.sample(5)
+      unique_albums = []
+      i = 0
+      until unique_albums.length == 20 || i == 20
+        if unique_albums.empty?
+          unique_albums << array[i]
+        else
+          # binding.pry
+          if !(unique_albums.any? {|e| e.name == array[i].name })
+            unique_albums << array[i]
+          end
+        end
+        i += 1
+      end
+      unique_albums
+      # binding.pry
+
     end
+
+
 
     def answer_params
 
-      params.permit(:release_year, :popularity_rate, :track, :release_year_id, :popularity_rate_id)
+      params.permit(:release_year, :popularity_rate, :track, :release_year_id, :popularity_rate_id, :sec_release_year_id, :sec_popularity_rate_id)
       # binding.pry
     end
 
--- app/models/album.rb
+++ app/models/album.rb
@@ -4,16 +4,24 @@ require 'rspotify'
 class Album
 
 
-  attr_reader :release_year, :popularity_rate, :track, :id
+  attr_reader :release_year, :popularity_rate, :track, :id, :track_popularity_rate
 
 
   def initialize(args)
 
     year = args.release_date.to_i
-    @release_year = {answer: year, question: "When was the track #{args.name} released?"}
+    @release_year = {answer: year, question: "When was #{args.name} released?"}
     @popularity_rate = {answer: args.popularity, question: "Guess #{args.name}'s popularity rate on Spotify?"}
-    track = args.tracks.sample.name
-    @track = {answer: track, question: "Is the song '#{track}' on the #{args.name} album?"}
+    track = args.tracks.sample
+    track_name = args.tracks.sample.name
+    track_popularity_rate = track.popularity
+
+    @track = {answer: track_name, question: "Is the song '#{track_name}' on the #{args.name} album?"}
+
+    @track_popularity_rate = {answer: track_popularity_rate, question: "Guess the song '#{track_name}'s popularity rate on Spotify?"}
+
+    # binding.pry
+
 
     @id = args.id
 
@@ -42,7 +50,12 @@ class Album
   # end
 
   def popularity_min_option
-    self.popularity_rate[:answer] + [-7, -2].to_a.sample
+    min = self.popularity_rate[:answer] + [-7, -2].to_a.sample
+    if min.negative?
+      return 0
+    else
+      return min
+    end
   end
 
   def popularity_max_option
@@ -50,6 +63,14 @@ class Album
     self.popularity_rate[:answer] + [2, before_max_100].to_a.sample
   end
 
+  def track_popularity_min_option
+    self.track_popularity_rate[:answer] + [-7, -2].to_a.sample
+  end
+
+  def track_popularity_max_option
+    before_max_100 = 97 - self.track_popularity_rate[:answer]
+    self.track_popularity_rate[:answer] + [2, before_max_100].to_a.sample
+  end
 
 
 end
